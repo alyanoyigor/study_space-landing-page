33e17e080c281caba9ca07ced02b43d00acbce1d
Igor <igoralyanoy@gmail.com>
2021-09-14 20:43:47 +0300

    feat: add main and footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-
+$(function () {
+	$(".slider__box").slick({
+		prevArrow:
+			'<img src="images/arrow-left.svg" alt="left-arrow" class="slider__arrow slider__arrow-left">',
+		nextArrow:
+			'<img src="images/arrow-right.svg" alt="right-arrow" class="slider__arrow slider__arrow-right">',
+	});
 });
